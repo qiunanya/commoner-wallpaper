2c4fae529c9d8c6516257c88f3c69834d3133bd5
布衣前端 <1766226354@qq.com>
2023-04-14 22:18:50 +0800

    引入electron-as-wallpaper依赖测试成功

--- electron-wallpaper/src/main/main.js
+++ electron-wallpaper/src/main/main.js
@@ -1,55 +1,65 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
+const {attach, detach, refresh} = require("electron-as-wallpaper");
+
 const path = require('path')
-const NODE_ENV = process.env.NODE_ENV
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    icon: path.resolve(
-      __dirname,
-      '../',
-      'asset',
-      'logo/icon.ico'
-    ),
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      webviewTag: true,
-      preload: path.join(__dirname, 'preload.js')
+let mainWindow;
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: path.resolve(
+            __dirname,
+            '../',
+            'asset',
+            'logo/icon.ico'
+        ),
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            webviewTag: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    // 开发环�?
+    if (!app.isPackaged) {
+        mainWindow.loadURL(path.resolve(
+            __dirname,
+            '../',
+            'renderer',
+            'index.html'
+        ))
+        // mainWindow.webContents.openDevTools()
+        console.log('开发环�?', app.isPackaged);
     }
-  })
-
-   // 开发环境
-   if (NODE_ENV === "development") {
-    mainWindow.loadURL('../renderer/index.html')
-    // mainWindow.webContents.openDevTools()
-  } 
-  else {
-      mainWindow.loadFile(
-        path.resolve(
-          __dirname,
-          '../',
-          'renderer',
-          'index.html'
+    // 生产�??
+    else {
+        console.log('生产�??', app.isPackaged);
+        mainWindow.loadFile(
+            path.resolve(
+                __dirname,
+                '../',
+                'renderer',
+                'index.html'
+            )
         )
-      )
-  }
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    }
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    createWindow()
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+    if (mainWindow) attach(mainWindow);
+    // detach(mainWindow);
 })
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
+    refresh();
 })
 
