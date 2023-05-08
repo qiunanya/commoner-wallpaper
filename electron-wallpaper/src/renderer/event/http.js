f60681fb76fb518cc2fa2e41db618368f40c5567
qiuny <1766226354@qq.com>
2023-05-08 17:42:48 +0800

    完成自然风景分页功能

--- electron-wallpaper/src/renderer/event/http.js
+++ electron-wallpaper/src/renderer/event/http.js
@@ -4,36 +4,71 @@ const openBtn = require("../ipc/open-child-win")
 
 const client = createClient('JFb8VVT9d8VLxWdjzY7lxs38njPTFeWe4FE9hktLk5LThVGxrQW3uJE4');
 const query = 'Nature';
+let currentPage = 1, totalPage=0, UL;
 // query: Nature、Tigers、People
 
-const parent = document.getElementById('pexels-list-view')
-client.photos.search({ query, per_page: 30 }).then(res => {
-    if (res) {
-        const { photos } = res;
-        const data = photos.map(mp => {
-            return {
-                imgUrl: mp.src.large,
-                tiny: mp.src.tiny,
-                small: mp.src.small
-            }
-        })
-        const UL = document.createElement('ul')
-            UL.setAttribute('class', 'wallpaper-list-widget')
-        for (let i = 0; i < data.length; i++) {
-            const LI = document.createElement('li')
-            LI.setAttribute('title', '设置为壁纸')
-            LI.setAttribute('id', 'pexelsImgItem')
-            const img = document.createElement('img')
-            img.src = data[i].small
-            img.setAttribute('id', 'pexelsImgItem')
-            LI.append(img)
-            UL.append(LI)
-            LI.onclick = function(event) {
-                // localStorage.setItem('currentImg', data[i].imgUrl)
-                openBtn.openChildWind(event, 'img', data[i].imgUrl)
-            }
-        }
-        parent.append(UL)
+const parent = document.getElementById('pexels-content-view')
+function getPageData() {
+    if (parent.hasChildNodes()) {
+        parent.removeChild(parent.firstChild)
     }
+    client.photos.search({ query, page: currentPage, per_page: 32 }).then(res => {
+        if (res) {
+            const { photos, total_results } = res;
+            totalPage = total_results / 32;
+            const data = photos.map(mp => {
+                return {
+                    imgUrl: mp.src.large,
+                    tiny: mp.src.tiny,
+                    small: mp.src.small
+                }
+            })
 
-});
+            UL = document.createElement('ul')
+            UL.setAttribute('class', 'wallpaper-list-widget')
+                
+            for (let i = 0; i < data.length; i++) {
+                const LI = document.createElement('li')
+                LI.setAttribute('title', '设置为壁纸')
+                LI.setAttribute('id', 'pexelsImgItem')
+                const img = document.createElement('img')
+                img.src = data[i].small
+                img.setAttribute('id', 'pexelsImgItem')
+                LI.append(img)
+                UL.append(LI)
+                LI.onclick = function(event) {
+                    // localStorage.setItem('currentImg', data[i].imgUrl)
+                    openBtn.openChildWind(event, 'img', data[i].imgUrl)
+                }
+            }
+            parent.appendChild(UL)
+        }
+    });
+}
+
+// 初始化请求数据
+getPageData()
+
+// 初始化分页参数
+const previous_page = document.getElementById('previous_page')
+const next_page = document.getElementById('next_page')
+const page_num = document.getElementById('page_num')
+
+page_num.innerText = currentPage;
+
+previous_page.onclick = function () {
+    if (currentPage > 1) {
+        currentPage--;
+        console.log(currentPage, 'pre');
+    }
+    page_num.innerText = currentPage;
+    getPageData()
+}
+next_page.onclick = function () {
+    if (currentPage && currentPage < totalPage) {
+        currentPage++;
+        console.log(currentPage, 'next');
+    }
+    page_num.innerText = currentPage;
+    getPageData()
+}
